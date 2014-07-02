90664ffd9baeba23486d5bc02e08f936e0b0bdb6
Brian Lambert <brianlambert@gmail.com>
2014-07-02 08:23:28 -0400

    Initial Commit

---
+++ NRDModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NRDModel
+//
+//  Created by Brian Lambert on 7/2/14.
+//  Copyright (c) 2014 Brian Lambert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NRDModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NRDModel
+//
+//  Created by Brian Lambert on 7/2/14.
+//  Copyright (c) 2014 Brian Lambert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
